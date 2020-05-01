c90683feafa65c001b80df68301946f60d24a9e4
helloc94 <1049990778@qq.com>
2020-05-01 20:13:04 +0800

    --- Signed-off-by: helloc94 <1049990778@qq.com>

--- spring-boot-eureka-server/src/main/java/com/neo/EurekaServerApplication.java
+++ spring-boot-eureka-server/src/main/java/com/neo/EurekaServerApplication.java
@@ -3,9 +3,9 @@ package com.neo;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
-//import org.springframework.security.config.annotation.web.builders.HttpSecurity;
-//import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
-//import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
 
 @EnableEurekaServer
 @SpringBootApplication
@@ -15,12 +15,12 @@ public class EurekaServerApplication {
         SpringApplication.run(EurekaServerApplication.class, args);
     }
 
-//    @EnableWebSecurity
-//    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
-//        @Override
-//        protected void configure(HttpSecurity http) throws Exception {
-//            http.csrf().disable();
-//            super.configure(http);
-//        }
-//    }
+    @EnableWebSecurity
+    public class WebSecurityConfigurer extends WebSecurityConfigurerAdapter {
+        @Override
+        protected void configure(HttpSecurity http) throws Exception {
+            http.csrf().disable();
+            super.configure(http);
+        }
+    }
 }
